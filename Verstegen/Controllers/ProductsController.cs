764d684c0dec43ede850f10d6710c5918018af3d
joost1998 <joostpoeldijk@hotmail.nl>
2018-12-06 16:05:39 +0100

    Upload project

---
+++ Verstegen/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Verstegen.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
---
+++ Verstegen/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Verstegen.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
