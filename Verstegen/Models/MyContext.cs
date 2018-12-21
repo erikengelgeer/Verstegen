db4f28893b80ee1519901821dc8652200a763115
LUMIAH <37831653+LUMIAH@users.noreply.github.com>
2018-12-21 15:33:48 +0100

    database aangemaakt

---
+++ Verstegen/Models/MyContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Verstegen.Models
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Recept> Recipes { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<ArticleAmount> ArticleAmounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=srv-h-se-wdpr-a.database.windows.net;Initial Catalog=grp2;User ID=grp2;Password=2osajposlwA;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+    }
+}
