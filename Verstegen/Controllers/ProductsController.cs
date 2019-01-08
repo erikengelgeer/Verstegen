7daebe03e9a035b93cdec80aa307f608c62cd8c8
erikengelgeer <erik-engelgeer@live.nl>
2019-01-08 16:16:23 +0100

    Producten toegevoegd (nog niet uit de database) aan de product overview pagina. Vorige knop toegevoegd en deze op disabled gezet.

--- Verstegen/Controllers/ProductsController.cs
+++ Verstegen/Controllers/ProductsController.cs
@@ -3,14 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Verstegen.Models;
 
 namespace Verstegen.Controllers
 {
     public class ProductsController : Controller
     {
+        readonly MyContext db;
+
+        public ProductsController()
+        {
+            db = new MyContext();
+        }
 
         public IActionResult Index()
         {
+            ViewBag.Contact = db.Contacts.OrderBy(c => Guid.NewGuid()).Skip(0).Take(1).First();
             return View();
         }
 
