6315c0a90823a2a4a91d578b85ad08490aa3fc2b
Thijs <thijsvanrheenenn@gmail.com>
2019-01-08 20:35:20 +0100

    Page T10 Afgemaakt, er moet alleen nog data uit de database gehaald worden

--- Verstegen/Controllers/RecipesController.cs
+++ Verstegen/Controllers/RecipesController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Verstegen.Models;
 
 namespace Verstegen.Controllers
 {
     public class RecipesController : Controller
     {
+        readonly MyContext db;
+
+        public RecipesController()
+        {
+            db = new MyContext();
+        }
+
         public IActionResult Index()
         {
+            ViewBag.Contact = db.Contacts.OrderBy(c => Guid.NewGuid()).Skip(0).Take(1).First();
             return View();
         }
     }
