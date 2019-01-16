11e80822ac1f7dc3aa230017ce7dde3c29dfcfc6
joost1998 <joostpoeldijk@hotmail.nl>
2019-01-16 18:25:26 +0100

    Fix

---
+++ ProductsTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+using Verstegen;
+using Verstegen.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Verstegen.Models;
+using Verstegen.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductsTest
+{
+    public class UnitTest1
+    {
+
+        //private ApplicationDbContext getContext()
+        //{
+            //var option = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase();
+
+            //return null;
+        //}
+
+        [Fact]
+        public void test1()
+        {
+
+        }
+    }
+}
