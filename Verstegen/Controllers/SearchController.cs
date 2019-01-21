cc8612e7315b75c9c652287e902d161d9d3f0cc9
joost1998 <joostpoeldijk@hotmail.nl>
2019-01-21 11:06:04 +0100

    hot-fix

--- Verstegen/Controllers/SearchController.cs
+++ Verstegen/Controllers/SearchController.cs
@@ -32,16 +32,24 @@ namespace Verstegen.Controllers
 
             ViewBag.Search = result;
 
+            //Haalt alle object op van de Model Blog
             var blogList = db.Blogs.Where(b => b.Title.Contains(result) || b.Subject.Contains(result) || b.SubTitle.Contains(result) || b.Text.Contains(result) || b.Quote.Contains(result) || b.QuoteFrom.Contains(result) || b.SubText.Contains(result) || b.Date.ToString().Contains(result) || b.Type.Contains(result)).ToList();
+            //Haalt alle object op van de Model Recipe
             var recipeList = db.Recipes.Where(r => r.Title.Contains(result) || r.SubTitle.Contains(result) || r.Type.Contains(result) || r.AmountOfPeople.Contains(result) || r.Procedure.Contains(result) || r.Decoration.Contains(result) || r.Ingredients.Any(a => a.Name.Contains(result) || a.Gram.ToString().Contains(result) || a.ArticleNumber.ToString().Contains(result) || a.Percentage.ToString().Contains(result))).ToList();
+            //Haalt alle object op van de Model Product
             var productList = db.Products.Where(p => p.Title.Contains(result) || p.Type.Contains(result) || p.Description.Contains(result) || p.Contents.Contains(result) || p.AltString.Contains(result) || p.ProductDetails.Contains(result)).ToList();
 
+            //Voegt de List met blogs toe aan de Object List
             resultList = (from x in blogList select (Object)x).ToList();
+            //Voegt de List met Recipes toe aan de Object List
             resultList.AddRange((from x in recipeList select (Object)x).ToList());
+            //Voegt de List met Products toe aan de Object List
             resultList.AddRange((from x in productList select (Object)x).ToList());
 
             ViewBag.TestList = resultList;
 
+            //Geeft alleen de benodigde Objecten mee aan de view en houd rekening mee met de pagina.
+            //De Functie ToPagedList() is een externe Package die de pagenavigation makkelijk maakt.
             ViewBag.resultList = resultList.ToPagedList(page, Amount);
             ViewBag.AmountPages = (int)Math.Ceiling((Double)resultList.Count() / Amount);
             ViewBag.Page = page;
